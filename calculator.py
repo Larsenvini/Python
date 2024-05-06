fe2b47825a311a7b989b985bfda4b12f1b720e6a
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-05-06 19:07:32 +0000

    Mon, May 6, 2024, 4:07 PM -03:00

---
+++ calculator.py
@@ -0,0 +1,10 @@
+def main ():
+    x = input("Type in your first number, please. ")
+    #this asks the user for first number of operation
+    y = input("Type in the second number, please. ")
+    #asks for users second number
+    z = round(x + y, 2)
+    return z
+
+main()
+
