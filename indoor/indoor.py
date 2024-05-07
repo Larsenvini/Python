525e9d03ab658ea2f5cdc91b07a4d3d3238aa63e
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-05-07 17:11:52 +0000

    Tue, May 7, 2024, 2:11 PM -03:00

--- indoor/indoor.py
+++ indoor/indoor.py
@@ -1,10 +1,10 @@
 def main():
-    name = str.lower(input("Please type in your favorite food and computer science course, separated only by a blank space. (example: banana cs50) "))
+    name = str.lower(input("Please type in your favorite food and computer science course, separated by a blank space. (example: banana cs50) "))
     name = name.strip(",")
     hello(name)
 
 def hello(para):
     first, last = para.split(" ")
-    print("Nice!", first, "Is a great food.", "And", last, "is a great course!!")
+    print("Nice!", first, "is a great food.", "And", last, "is a great course!!")
 
 main()
