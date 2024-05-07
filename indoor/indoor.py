115d59931bfc56bc1994b9d6149ea575a16cdc14
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-05-07 17:14:28 +0000

    Tue, May 7, 2024, 2:14 PM -03:00

--- indoor/indoor.py
+++ indoor/indoor.py
@@ -1,10 +1,8 @@
 def main():
-    name = str.lower(input("Please type in your favorite food and computer science course, separated by a blank space. (example: banana cs50) "))
-    name = name.strip(",")
+    name = str.lower(input("Say something in caps lock"))
     hello(name)
 
 def hello(para):
-    first, last = para.split(" ")
-    print("Nice!", first, "is a great food.", "And", last, "is a great course!!")
+    print("")
 
 main()
