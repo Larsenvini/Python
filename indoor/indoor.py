4101d741f49bc69a1f6617d733f945be4f75ae4c
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-05-07 17:02:44 +0000

    Tue, May 7, 2024, 2:02 PM -03:00

--- indoor/indoor.py
+++ indoor/indoor.py
@@ -1,5 +1,5 @@
 def main():
-    name = str.lower(input("Whats is your favorite food and computer science course? "))
+    name = str.lower(input("Whats is your favorite food and computer science course? ")).strip(",")
     hello(name)
 
 def hello(para):
