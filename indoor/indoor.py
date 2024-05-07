e89c00559b2529f7ae66031a642ffe354696177a
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-05-07 17:00:04 +0000

    Tue, May 7, 2024, 2:00 PM -03:00

--- indoor/indoor.py
+++ indoor/indoor.py
@@ -1,9 +1,9 @@
 def main():
     name = str.lower(input("Whats is your favorite food and computer science course? "))
-    first, last = name.split(" ")
     hello(name)
 
 def hello(para):
-    print("Nice!", first, "Is a great food.", "And", last, "is a great course!!")
+    first, last = para.split(" ")
+    print(f"Nice!", {first}, "Is a great food.", "And", {last}, "is a great course!!")
 
 main()
