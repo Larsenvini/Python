f5f32d7946865a241d4e80e548d1888acae0548b
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-05-13 17:57:12 +0000

    Mon, May 13, 2024, 2:57 PM -03:00

--- faces/faces.py
+++ faces/faces.py
@@ -4,3 +4,8 @@ def cs50translate(text, translation_dict ):
     return text
 
 text_to_translate = input()
+translation_dict = {':)' : "🙂", ':(': "🙁"}
+
+translated_text = cs50translate(text_to_translate, translation_dict)
+
+print(translated_text)
