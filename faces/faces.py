2a3554619216ae09e021e091a219e354efc5e98f
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-05-13 17:52:33 +0000

    Mon, May 13, 2024, 2:52 PM -03:00

--- faces/faces.py
+++ faces/faces.py
@@ -1,2 +1,6 @@
 def cs50translate(text, translation_dict ):
-    for key, value in translation_dict.items
+    for key, value in translation_dict.items:
+        text = text.replace(key, value)
+    return text
+
+text_to_translate = input()
