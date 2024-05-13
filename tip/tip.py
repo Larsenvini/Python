744b633f52796c33bfc089aab784420f0b06716a
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-05-13 21:10:39 +0000

    Mon, May 13, 2024, 6:10 PM -03:00

--- tip/tip.py
+++ tip/tip.py
@@ -2,7 +2,7 @@ def main():
     dollars = dollars_to_float(input("How much was the meal? "))
     percent = percent_to_float(input("What percentage would you like to tip? "))
     tip = dollars * percent
-    rounded_tip = tip(round, 2)
+    rounded_tip = round(tip, 2)
     print("Leave ${0}".format(rounded_tip))
 
 
