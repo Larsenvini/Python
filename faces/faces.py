3473faa1f7e37d75596f647354306aec74ce96e2
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-05-13 17:38:20 +0000

    Mon, May 13, 2024, 2:38 PM -03:00

--- faces/faces.py
+++ faces/faces.py
@@ -1,4 +1,4 @@
-translation_table = input().maketrans({':)', '🙂.', ':(', '🙁'})
-original_string = ':)'
+translation_table = str.maketrans({':)', '🙂.', ':(', '🙁'})
+original_string = input()
 translated_string = original_string.translate(translation_table)
 print(translated_string)
