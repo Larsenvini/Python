e805c7f1e021df1d8182056287f427da8ff72c10
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-05-13 19:54:58 +0000

    Mon, May 13, 2024, 4:54 PM -03:00

--- eisten/eisten.py
+++ eisten/eisten.py
@@ -1,8 +1,8 @@
 from operator import mul
 def calculator():
-    m = input("Welcome to Vini's EnergyCalculator! Type a mass: ")
+    m = int(input("Welcome to Vini's EnergyCalculator! Type a mass: "))
     c = 300000000
-    e = mul('m',pow('c',2))
+    e = mul(m, pow(c, 2))
 
     print(e)
 
