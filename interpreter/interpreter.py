e98f88210eb4f38b5d7213177d7802b8bc7346ad
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-05-14 17:50:42 +0000

    Tue, May 14, 2024, 2:50 PM -03:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,8 +1,13 @@
 expression = input("Expression: ")
 x, y, z = expression.split(" ")
 
+x = int(x)
+z = int(z)
 
-formatted_expression = "${:.1f}".format(expression)
+if y == "+":
+    print(x + z)
+
+    formatted_expression = "${:.1f}".format(expression)
 print(f"Leave{formatted_expression}")
 
 
