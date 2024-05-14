9149f8c517834f9f10f9a3b5b9304cf525c35950
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-05-14 22:39:20 +0000

    Tue, May 14, 2024, 7:39 PM -03:00

--- meal/meal.py
+++ meal/meal.py
@@ -3,12 +3,12 @@ def main():
     converted_time = convert(meal)
 
     if 7 <= converted_time and converted_time <= 8:
-        print("breakfast")
+        print("breakfast time")
 
     elif 12 <= converted_time and converted_time <= 13:
-            print("lunch")
+            print("lunch time")
     elif 18 <= converted_time and converted_time <= 19:
-            print("dinner")
+            print("dinner time")
     else:
             print(" ")
 
