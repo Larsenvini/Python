8eb9213afe3332f5f86ca7844e835df1dacb1c79
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-05-14 18:06:05 +0000

    Tue, May 14, 2024, 3:06 PM -03:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -5,11 +5,19 @@ x = int(x)
 z = int(z)
 
 if y == "+":
-    print("${:.1f}".format(x + z))
+    print("{:.1f}".format(x + z))
 
 elif y == "-":
-    print("${:.1f}".format(x - z))
+    print("{:.1f}".format(x - z))
 
+elif y == "*":
+    print("{:.1f}".format(x * z))
+
+elif y == "/":
+    print("{:.1f}".format(x / z))
+
+else:
+    print("ERROR")
 
 
 
