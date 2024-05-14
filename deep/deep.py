39357621f87060eb699a1cfcc9ce9d1e4b95932c
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-05-14 14:07:42 +0000

    Tue, May 14, 2024, 11:07 AM -03:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,9 +1,9 @@
-the_answer = input("What is the answer to the Great Question of Life, the Universe and Everything? ")
+the_answer = input("What is the answer to the Great Question of Life, the Universe and Everything? ").strip()
 
 match the_answer:
     case "42" | "forty two" | "forty-two":
         print("Yes")
     case _:
-        print("no")
+        print("No")
 
 
