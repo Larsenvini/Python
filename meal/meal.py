bf7f7477964a00b0fdd396481ae22744320c4a01
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-05-14 20:09:50 +0000

    Tue, May 14, 2024, 5:09 PM -03:00

--- meal/meal.py
+++ meal/meal.py
@@ -2,12 +2,16 @@ def main():
     meal = input("What time is it? ")
     convert(meal)
 
+
 #:## a.m. and ##:## a.m.
 #:## p.m. and ##:## p.m.
 
 
 def convert(time):
-    ...
+    hours, minutes = time.split(":")
+    
+
+main()
 
 
 if __name__ == "__main__":
