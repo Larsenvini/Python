e2181fdbebb6b2d38b1b8a8bb54c2b710e0fba84
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-05-14 16:58:53 +0000

    Tue, May 14, 2024, 1:58 PM -03:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,6 +1,25 @@
 mediatype = input("File name: ")
-ext_list = (".gif", ".jpg", ".jpeg", ".png", ".pdf", ".zip", ".txt")
 def main():
-    i
+    match mediatype:
+        case ".gif":
+            print("image/gif")
 
+        case ".jpg" | ".jpeg":
+            print("image/jpeg")
 
+        case ".png":
+            print("image/png")
+
+        case ".pdf":
+            print("image/pdf")
+
+        case ".txt":
+            print("image/txt")
+
+        case ".zip":
+            print("image/zip")
+
+        case _:
+            print("application/octet-stream")
+
+main()
