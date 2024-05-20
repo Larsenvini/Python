4a0a9bf3fded1b47499678b3698547fb710db81d
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-05-20 22:45:48 +0000

    Mon, May 20, 2024, 7:45 PM -03:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,6 +1,6 @@
 amount_due = 50
 while amount_due > 0:
-    print("Amount due:"(amount_due))
+    print("Amount due:", amount_due)
     pay = input("Insert coin: ")
     if pay == 25:
         amount_due = amount_due - 25
@@ -13,5 +13,9 @@ while amount_due > 0:
     else:
         print("Value not accepted")
 
-        
+    if amount_due <= 0:
+        break
+    print("Change Owed:", pay)
+
+
 
