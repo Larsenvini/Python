3651a3dc3f6ad7ab955a19e80bcdbfaf11e9d9d4
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-05-20 16:54:10 +0000

    Mon, May 20, 2024, 1:54 PM -03:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,7 +1,7 @@
 def main():
-    convert = input(": ")
-    converted = add_(convert)
-    print (converted)
+    convert = input(": ") #get input
+    converted = add_(convert) #transform uppercase in lower and add _ before originally uppercased letters
+    print (converted)#print the converted string
 
 def add_(arroba):
     return "".join('_' + i if i.isupper() else i for i in arroba).lower()
