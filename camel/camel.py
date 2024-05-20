32acc72d1b818829ec53b46cd4a95b2ce911713a
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-05-20 16:32:50 +0000

    Mon, May 20, 2024, 1:32 PM -03:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,7 +1,7 @@
 def main():
-    convert = input(": ").lower()
+    convert = input(": ")
     add_(convert)
-    print (convert)
+    print (convert.lower())
 
 def add_(s):
     s.join('_' + i if i.isupper() else i for i in s)
