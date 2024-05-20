700bcd37be59093b3800dc893eb0c14b5189e12e
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-05-20 16:39:16 +0000

    Mon, May 20, 2024, 1:39 PM -03:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,11 +1,10 @@
 def main():
-    convert = input(": ")
+    convert = input(": ").lower()
     converted = add_(convert)
     print (converted)
 
 def add_(arroba):
     "".join('_' + i if i.isupper() else i for i in arroba)
-    arroba.lower()
     return arroba
 
 main()
