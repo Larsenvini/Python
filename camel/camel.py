2511a91c0b55bf3c7d00a2d2717e799b3981615f
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-05-20 15:53:50 +0000

    Mon, May 20, 2024, 12:53 PM -03:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,6 +1,6 @@
 def main():
-    for convert in input("Type something in camel case: "):
-        print(convert, end = "").lower()
+    for convert in input("Type something in camel case: ").lower():
+        print(convert, end = "")
 
 
 
