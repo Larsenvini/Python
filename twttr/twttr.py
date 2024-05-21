90174564bfaf489617897f55d9634db111cfdce6
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-05-21 21:37:01 +0000

    Tue, May 21, 2024, 6:37 PM -03:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,6 +1,10 @@
 giria = input("Input: ")
-vogals = ["a", "e", "i", "o", "u"]
-giria = giria.strip(vogals)
+vogals = {"a" : "",
+           "e" : "",
+             "i": "",
+               "o" : "",
+                 "u" : ""}
+giria = giria.translate(vogals)
 print (giria)
 
 
