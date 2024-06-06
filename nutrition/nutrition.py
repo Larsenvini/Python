8727ed572232aa21f355ced43d9958e64e0f4b55
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-06-06 03:13:52 +0000

    Thu, Jun 6, 2024, 12:13 AM -03:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,33 +1,30 @@
-fruit = input("Input: ")
-calories = {"Apple" : "130",
-           "Avocado California" : "50",
-             "Banana": "110",
-               "Cantaloupe " : "50",
-                 "Grapefruit" : "60",
-                 "Grapes" : "90",
-                 "Honeydew Melon": "50",
-                 "Kiwifruit":"90",
-                 "Lemon":"15",
-                 "Lime":"20",
-                 "Nectarine":"60",
-                 "Orange ":"80",
-                 "Peach":"60",
-                 "Pear":"100",
-                 "Pineapple":"50",
-                 "Plums":"70",
-                 "Strawberries":"50",
-                 "Sweet Cherries":"100",
-                 "Tangerine":"50",
-                 "Watermelon":"80",
-                 }
 
-# Create a translation table for fruit names to their respective calorie counts
-translation_table = str.maketrans(calories)
+calories = { "Apple" : "130",
+             "Avocado California" : "50",
+             "Banana": "110",
+             "Cantaloupe " : "50",
+             "Grapefruit" : "60",
+             "Grapes" : "90",
+             "Honeydew Melon": "50",
+             "Kiwifruit":"90",
+             "Lemon":"15",
+             "Lime":"20",
+             "Nectarine":"60",
+             "Orange ":"80",
+             "Peach":"60",
+             "Pear":"100",
+             "Pineapple":"50",
+             "Plums":"70",
+             "Strawberries":"50",
+             "Sweet Cherries":"100",
+             "Tangerine":"50",
+             "Watermelon":"80" }
 
 fruit = input("Input a fruit name: ")
 
-# Use the translation table with the translate method
-calories_count = fruit.translate(translation_table)
-
-print(calories_count)
+if fruit in calories:
+    calories_count = calories[fruit]
+    print(calories_count)
+else:
+    print("")
 
