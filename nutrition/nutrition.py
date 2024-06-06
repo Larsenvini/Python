0258f03edb0e67614f91514fd9f2e31bfef68602
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-06-06 02:49:50 +0000

    Wed, Jun 5, 2024, 11:49 PM -03:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -7,7 +7,19 @@ calories = {"Apple" : "130",
                  "Grapes" : "90",
                  "Honeydew Melon": "50",
                  "Kiwifruit":"90",
+                 "Lemon":"15",
+                 "Lime":"20",
+                 "Nectarine":"60",
+                 "Orange ":"80",
+                 "Peach":"60",
+                 "Pear":"100",
+                 "Pineapple":"50",
+                 "Plums":"",
+                 "Strawberries":"",
                  "":"",
+                 "":"",
+                 "":"",
+
                  }
 
 per_fruit = fruit.maketrans(calories)
