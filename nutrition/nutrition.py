2b7f6498acf0bc958daa99c9bff92643ba49b72b
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-06-06 02:48:26 +0000

    Wed, Jun 5, 2024, 11:48 PM -03:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,10 +1,14 @@
 fruit = input("Input: ")
-calories = {"a" : "",
-           "e" : "",
-             "i": "",
-               "o" : "",
-                 "u" : "",
-                 "O" : ""}
+calories = {"Apple" : "130",
+           "Avocado California" : "50",
+             "Banana": "110",
+               "Cantaloupe " : "50",
+                 "Grapefruit" : "60",
+                 "Grapes" : "90",
+                 "Honeydew Melon": "50",
+                 "Kiwifruit":"90",
+                 "":"",
+                 }
 
 per_fruit = fruit.maketrans(calories)
 
