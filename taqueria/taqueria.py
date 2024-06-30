8fdc1b7524da343659f6d549113d0757ebe54047
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-06-30 22:27:08 +0000

    Sun, Jun 30, 2024, 7:27 PM -03:00

---
+++ taqueria/taqueria.py
@@ -0,0 +1,11 @@
+{
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
