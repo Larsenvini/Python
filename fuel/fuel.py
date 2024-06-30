296292025b8a2276d79ab9790df10f47a5c06afa
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-06-30 21:39:36 +0000

    Sun, Jun 30, 2024, 6:39 PM -03:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,6 +1,11 @@
 def main():
     fuel = get_fraction()
-    print(fuel)
+    if fuel == "1/4":
+        print("25%")
+    elif fuel == "2/4":
+        print("50%")
+    elif fuel == "3/4":
+        print("75%")
 
 def get_fraction():
     try:
