c0fbc3720be64d64aa9d893fb5832c596968e940
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-06-30 22:04:18 +0000

    Sun, Jun 30, 2024, 7:04 PM -03:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,15 +1,10 @@
 def main():
-    fuel = get_fraction("Fraction: ")
-    if fuel == "1/4":
-        print("25%")
-    elif fuel == "2/4":
-        print("50%")
-    elif fuel == "3/4":
-        print("75%")
-    elif fuel == "4/4":
-        print("F")
-    elif fuel == "0/4":
-        print("E")
+    fraction = input("Fraction: ")
+    try:
+        percentage = convert_to_percentage(fraction)
+        if percentage <= 1:
+            print("E")
+            elif percentage 
 
 
 def get_fraction(prompt):
