ebaa56c83ab971a734680eb1d54a150cb7b8bc34
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-02 15:03:56 +0000

    Tue, Jul 2, 2024, 12:03 PM -03:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -3,7 +3,10 @@ grocery_list = {}
 while True:
     try:
         item = input().strip().lower()
-        if item 
+        if item in grocery_list:
+            grocery_list[item] += 1
+        else:
+            grocery_list[item] = 1
     except EOFError:
         break
 
