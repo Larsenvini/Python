ab6090bd8a7a01be3e0fe271551bc3a461b333c8
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-03 03:38:17 +0000

    Wed, Jul 3, 2024, 12:38 AM -03:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -24,4 +24,8 @@ def get_date():
         if match:
             month, day, year = match.groups()
             try:
-                
+                return f"{int(year):04d}-{int(month):02d}-{int(day):02d}"
+            except ValueError:
+                pass
+
+            
