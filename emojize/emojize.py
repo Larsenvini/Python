c9a4662e885ae0f8e588d70a0fb9fb303f70252b
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-03 22:18:22 +0000

    Wed, Jul 3, 2024, 7:18 PM -03:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -7,7 +7,7 @@ try:
     elif len(sys.argv) > 2:
         print('too many arguments')
     else:
-        print('hello my name is', sys.argv[1])
+        cowsay.trex('hello my name is', sys.argv[1])
 
 except:
-    print('Contact customer service')
+    cowsay.cow('Contact customer service')
