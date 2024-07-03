192ca00201166e075f825298d53ec7d7d490884d
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-03 22:44:37 +0000

    Wed, Jul 3, 2024, 7:44 PM -03:00

--- itunes/itunes.py
+++ itunes/itunes.py
@@ -4,4 +4,4 @@ import sys
 if len(sys.argv) != 2:
     sys.exit()
 
-requests.get("https://itunes.apple.com/search/?entity=song&)
+requests.get("https://itunes.apple.com/search/?entity=song&limit=1&term=weezer)
