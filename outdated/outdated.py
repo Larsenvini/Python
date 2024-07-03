a5c6430db5f83145653bad0b8b4ee62f35697365
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-03 02:54:31 +0000

    Tue, Jul 2, 2024, 11:54 PM -03:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -1,30 +1,3 @@
-months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-]
-month, day, year = date.split)"/"
-while True:
-    try:
-        date = input().strip().lower()
-        if date in grocery_list:
-            grocery_list[item] += 1
-        else:
-            grocery_list[item] = 1
-    except EOFError:
-        break
+import re
 
-sorted_items = sorted(grocery_list.keys())
-
-
-for item in sorted_items:
-    print(f"{grocery_list[item]} {item.upper()}")
+months =
