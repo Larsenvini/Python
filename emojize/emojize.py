b7acce4993b05334adf1dae442842d0597b45286
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-04 17:48:40 +0000

    Thu, Jul 4, 2024, 2:48 PM -03:00

--- emojize/emojize.py
+++ emojize/emojize.py
@@ -1,13 +1,2 @@
-import cowsay
-import sys
+emoji_code = input("Input: ")
 
-try:
-    if len(sys.argv) < 2:
-        print('too few arguments')
-    elif len(sys.argv) > 2:
-        print('too many arguments')
-    else:
-        cowsay.trex('hello my name is', sys.argv[1])
-
-except:
-    cowsay.cow('Contact customer service')
