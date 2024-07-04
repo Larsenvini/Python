89f1f7562d5373d976cac7539de1744e264095df
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-04 19:14:23 +0000

    Thu, Jul 4, 2024, 4:14 PM -03:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -5,6 +5,10 @@ import random
 def main():
     available_fonts = pyfiglet.FigletFont.getFonts()
 
-    if len(sys.argv)
+    if len(sys.argv) == 1:
+        font = random.choice(available_fonts)
+
+    elif len(sys.argv) == 3:
+        if a
 
 
