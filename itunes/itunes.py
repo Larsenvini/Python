c9b8ee608cf44e50ad2ca7c609efeb30fbe28377
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-04 00:18:50 +0000

    Wed, Jul 3, 2024, 9:18 PM -03:00

--- itunes/itunes.py
+++ itunes/itunes.py
@@ -1,9 +1,9 @@
 import requests
 import sys
 
-if len(sys.argv) != 2:
-    sys.exit()
+def main():
+    if len(sys.argv) != 2:
+        sys.exit()
 
 response = requests.get("https://itunes.apple.com/search/?entity=song&limit=1&term=" + sys.argv[1])
-
 print(response.json())
