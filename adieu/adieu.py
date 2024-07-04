23eda364d5239268b18dd8929a6857229d88352f
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-04 19:57:45 +0000

    Thu, Jul 4, 2024, 4:57 PM -03:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -4,3 +4,13 @@ p = inflect.engine()
 
 def main():
     names = []
+
+    while True:
+        try:
+            name = input("Name: ").strip()
+            if name:
+                names.append(name)
+        except EOFError:
+            pass
+
+    formatted_names = p.join(names)
