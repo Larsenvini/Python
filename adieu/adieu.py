9da314429599f699c81b90446a97d99e580c8308
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-04 20:00:26 +0000

    Thu, Jul 4, 2024, 5:00 PM -03:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -5,12 +5,17 @@ p = inflect.engine()
 def main():
     names = []
 
-    while True:
-        try:
+    try:
+        while True:
             name = input("Name: ").strip()
             if name:
                 names.append(name)
-        except EOFError:
+    except EOFError:
             pass
 
     formatted_names = p.join(names)
+
+    print(f"Adieu, adieu, to {formatted_names}")
+
+if __name__ == "__main__":
+    main()
