80e0d78d6c7052aac15e0f3531497e5fcafae809
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-06 19:02:08 +0000

    Sat, Jul 6, 2024, 4:02 PM -03:00

--- test_calculator.py
+++ test_calculator.py
@@ -2,4 +2,7 @@ from calculator import square
 
 def test_square():
     if square(2) != 4:
+        print("2 squared did not equal 4")
+    if square(3) != 9:
+        print("3 squared did not equal 9")
 
