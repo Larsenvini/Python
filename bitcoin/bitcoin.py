464bfb96588ba8abb855962681f3d08433f41010
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-06 14:41:14 +0000

    Sat, Jul 6, 2024, 11:41 AM -03:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -10,7 +10,7 @@ def main():
 
 
 
-            print(converted_response)
+            print(f"${converted_response:,.4f}")
         except requests.RequestException:
             sys.exit()
     ...
