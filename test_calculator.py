d525fee36958ee4f6debb0d88c2173e56efd4dc1
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-06 20:13:29 +0000

    Sat, Jul 6, 2024, 5:13 PM -03:00

--- test_calculator.py
+++ test_calculator.py
@@ -4,18 +4,11 @@ def main():
     test_square()
 
 def test_square():
-    try:
-        assert square(2) == 4
-    except AssertionError:
-        print("boo")
-    try:
-        assert square(3) == 9
-    except AssertionError:
-        print("boo")
-    try:
-        assert square(10) == 100
-    except AssertionError:
-        print("boo")
+    assert square(2) == 4
+
+    assert square(3) == 9
+
+    assert square(10) == 100
 
 
 if __name__ == '__main__':
