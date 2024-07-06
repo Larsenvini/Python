3eff66d31e1313c600e739fd214e16e22e229940
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-06 14:21:51 +0000

    Sat, Jul 6, 2024, 11:21 AM -03:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -1,13 +1,24 @@
 import requests
 import sys
 
-def convert():
-    
+def get_amount(response):
+    response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json" + sys.argv[1])
+    return response
+
+def convert(response):
+    while True:
+        try:
+         converted_response = response.float 
+
+        except requests.RequestException:
+            sys.exit()
+    ...
 
 def main():
     while True:
         try:
-            response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json" + sys.argv[1])
+
+
 
             print(response.json())
         except requests.RequestException:
