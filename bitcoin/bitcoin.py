1561b7f18cbbbdaed3faf8c891c7437be60412c2
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-06 14:10:52 +0000

    Sat, Jul 6, 2024, 11:10 AM -03:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -1,4 +1,12 @@
 import requests
+import sys
+
+if len(sys.argv) != float:
+    sys.exit()
+
+response = requests.get("https://itunes.apple.com/search/?entity=song&limit=1&term=" + sys.argv[1])
+
+print(response.json())
 
 def main()
     while True:
