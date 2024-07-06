1d4f7eb474e8317cf274ea156adc0100e2128889
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-06 19:54:21 +0000

    Sat, Jul 6, 2024, 4:54 PM -03:00

--- test_calculator.py
+++ test_calculator.py
@@ -4,14 +4,12 @@ def main():
     test_square()
 
 def test_square():
-    if square(2) != 4:
-        print("2 squared did not equal 4")
-    if square(3) != 9:
-        print("3 squared did not equal 9")
-    if square(10) != 100:
-        print("10 squared did not equal 100")
-    else:
-        print("tudo certo amg")
+    assert square(2) == 4
+
+    assert square(3) == 9
+
+    assert square(10) == 100
+
 
 if __name__ == '__main__':
     main()
