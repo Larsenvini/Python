375a6de1b713fe74535fd19fb60d7cac65bff128
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-06 22:22:41 +0000

    Sat, Jul 6, 2024, 7:22 PM -03:00

--- test_calculator.py
+++ test_calculator.py
@@ -1,15 +1,25 @@
 from calculator import square
 
 def main():
-    test_square()
+    test_positive()
+    test_negative()
+    test_zero()
+    test_decimal()
 
-def test_square():
+def test_positive():
     assert square(2) == 4
 
     assert square(3) == 9
 
-    assert square(10) == 100
+def test_negative():
+    assert square(-10) == 100
+    assert square(-5) == 25
 
+def test_zero():
+    assert square(0) == 0
+
+def test_decimal():
+    assert square(1.5) == 2.25
 
 if __name__ == '__main__':
     main()
