230deb395d022658a24895da7cc25f621a7f2ffd
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-07 23:09:57 +0000

    Sun, Jul 7, 2024, 8:09 PM -03:00

--- names.py
+++ names.py
@@ -1,8 +1,7 @@
 names = []
 
 for _ in range(3):
-    names.append(input("Whats your name? "))
+    names.append(input("Whats your name? ").capitalize())
 
-
-
-
+for name in sorted(names):
+    print(f"Hello, {name}")
