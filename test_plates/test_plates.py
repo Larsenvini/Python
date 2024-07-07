48d79564bdb2395e39c570db759b37d1b7db311e
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-07 13:26:22 +0000

    Sun, Jul 7, 2024, 10:26 AM -03:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -4,21 +4,8 @@ from plates import is_valid
 def test_validornah():
     assert is_valid("OPAH2340") == False
     assert is_valid("ROB776") == False
-    assert value("Hello there") == 0
-
-def test_h():
-    assert value("hi") == 20
-    assert value("hey") == 20
-
-def test_other():
-    assert value("good morning") == 100
-
-def test_case_insensitivity():
-    assert value("HELLO") == 0
-    assert value("HeLLo") == 0
-    assert value("hI") == 20
-    assert value("HAPPY") == 20
-
+    assert is_valid("CS50") == True
+    assert is_valid("HUBA7") == True
 
 if __name__ == "__main__":
     pytest.main()
