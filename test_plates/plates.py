eefebcab9341834e5619c59856d1e81d0f984b57
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-07 14:07:00 +0000

    Sun, Jul 7, 2024, 11:07 AM -03:00

--- test_plates/plates.py
+++ test_plates/plates.py
@@ -2,28 +2,30 @@ def main():
     plate = input("Vanity Plate: ")
     if is_valid(plate):
         print("Valid")
+        return 0  # Returning 0 to indicate success
     else:
         print("Invalid")
+        return 1  # Returning non-zero to indicate failure
 
 def is_valid(s):
-     if len(s) != 6:
-        return False
-     
-     half_lenght = len(s) // 2
-
-     part1 = s[:half_lenght]
-     part2 = s[half_lenght:]
-
-     if not part1.isalpha() or len(part1) != 2:
+    if len(s) != 6:
         return False
 
-     if not part2.isdigit() or len(part2) != 2:
+    half_length = len(s) // 2
+    part1 = s[:half_length]
+    part2 = s[half_length:]
+
+    if not part1.isalpha() or len(part1) != 2:
         return False
 
-     if '0' in part2:
-         return False
+    if not part2.isdigit() or len(part2) != 2:
+        return False
 
-     return True
+    if '0' in part2:
+        return False
+
+    return True
 
 if __name__ == "__main__":
-    main()
+    import sys
+    sys.exit(main())
