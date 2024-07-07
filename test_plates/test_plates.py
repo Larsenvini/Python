a62e147759da234fe7bef0e6b16d5498ef6c7375
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-07 13:38:30 +0000

    Sun, Jul 7, 2024, 10:38 AM -03:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -1,11 +1,17 @@
 import pytest
 from plates import is_valid
 
-def test_plates():
+def test_plate1():
     assert is_valid("OPAH2340") == False
+def test_plate2():
     assert is_valid("ROB776") == False
+def test_plate3():
     assert is_valid("CS50") == True
+def test_plate4():
     assert is_valid("HUBA7") == True
+
+def test_platestr():
+    with pytest.raises()
 
 if __name__ == "__main__":
     pytest.main()
