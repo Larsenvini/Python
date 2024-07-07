793bdabae5cb1c1a0d411991ac72e04dba71636c
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-07 00:38:00 +0000

    Sat, Jul 6, 2024, 9:38 PM -03:00

--- hello.py
+++ hello.py
@@ -2,6 +2,9 @@ def main():
     name = input("What's your name? ").lower().capitalize()
     hello(name)
 
-def hello(name):
-     print("Hello,", name)
-main()
+def hello(to = "World"):
+     print("Hello,", to)
+
+if __name__ == "__main__":
+     main()
+
