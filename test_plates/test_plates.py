9fd4dfabf7d48e4a831301580d462852d38cc274
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-07 13:20:10 +0000

    Sun, Jul 7, 2024, 10:20 AM -03:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -1,1 +1,24 @@
-ode 
+import pytest
+from plates import is_valid
+
+def test_():
+    assert value("Hello") == 0
+    assert value("hello") == 0
+    assert value("Hello there") == 0
+
+def test_h():
+    assert value("hi") == 20
+    assert value("hey") == 20
+
+def test_other():
+    assert value("good morning") == 100
+
+def test_case_insensitivity():
+    assert value("HELLO") == 0
+    assert value("HeLLo") == 0
+    assert value("hI") == 20
+    assert value("HAPPY") == 20
+
+
+if __name__ == "__main__":
+    pytest.main()
