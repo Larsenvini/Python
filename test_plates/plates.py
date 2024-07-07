72defd5cb62c74fec49bb418d43ece5a3cf08041
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-07 13:16:05 +0000

    Sun, Jul 7, 2024, 10:16 AM -03:00

---
+++ test_plates/plates.py
@@ -0,0 +1,24 @@
+def main():
+    plate = input("Vanity Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+def is_valid(algo):
+    half_lenght = len(algo) // 2
+    alg1 = algo[:half_lenght]
+    alg2 = algo[half_lenght:]
+    if alg1.isalpha():
+
+        if len(alg1) == 2:
+            return True
+        else:
+            return False
+    if alg2.isdigit():
+        if len(alg2) == 2:
+            return True
+        else:
+            return False
+
+main()
