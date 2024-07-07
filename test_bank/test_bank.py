227fefbd1e413e67e622f9f3825183443ba3b457
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-07 12:49:59 +0000

    Sun, Jul 7, 2024, 9:49 AM -03:00

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -1,18 +1,18 @@
 import pytest
-from bank import greet
+from bank import value
 
 def test_hello(msgn):
     if msgn.startswith("hello"):
-        assert greet(msgn) == "$0"
+        assert value(msgn) == "$0"
 
 def test_h(msgn):
     if msgn.startswith("h"):
-        assert greet(msgn) == "$20"
+        assert value(msgn) == "$20"
 
 def test_other(msgn):
     if test_hello() and test_h() == False:
-        assert greet(msgn) == "$100"
+        assert value(msgn) == "$100"
 
 def test_int():
      with pytest.raises(TypeError):
-          greet("2")
+          value("2")
