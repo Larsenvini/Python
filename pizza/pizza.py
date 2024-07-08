55093820e1b069927568ecdbd0fd18e3994df4b1
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-08 22:50:45 +0000

    Mon, Jul 8, 2024, 7:50 PM -03:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -1,9 +1,20 @@
 import tabulate
 import sys
+import os
 
 def main():
     if len(sys.argv) != 2:
         sys.exit("Usage: python pizza.py <filename.py>")
 
 
-        filename = sys.argv[1]
+    filename = sys.argv[1]
+
+    if not filename.endswith(".csv"):
+        sys.exit("Not a csv file")
+
+    if not os.path.isfile(filename):
+        sys.exit("File does not exist")
+
+    try:
+        for 
+        grid(filename)
