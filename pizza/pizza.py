248a8c4e6393e76585e43a589d1fce39d065fbf8
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-08 22:48:04 +0000

    Mon, Jul 8, 2024, 7:48 PM -03:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -1,1 +1,9 @@
-from tabulate import tabulate
+import tabulate
+import sys
+
+def main():
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python pizza.py <filename.py>")
+
+
+        filename = sys.argv[1]
