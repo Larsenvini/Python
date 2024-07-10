0e1d811734cb2d9b0ff426994358797abb153c54
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-10 19:42:50 +0000

    Wed, Jul 10, 2024, 4:42 PM -03:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -1,2 +1,8 @@
 import pytest
 from numb3rs import validate
+
+def test_true():
+    assert validate("255.255.255.255") == True
+    assert validate("512.512.512.512") == True
+    assert validate("127.0.0.1") == True
+    assert validate("12.0.0.1") == True
