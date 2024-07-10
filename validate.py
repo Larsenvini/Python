5b1efd1f65773268c82352ae4235991ef8bfd0f5
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-10 21:41:32 +0000

    Wed, Jul 10, 2024, 6:41 PM -03:00

--- validate.py
+++ validate.py
@@ -2,7 +2,7 @@ import re
 
 email = input("What's your email? ").strip()
 
-if re.search(r'^\w+@(\2+\.)?\2+\.edu$', email, re.IGNORECASE):
+if re.search(r"^\w+@(\w+\.)?\w+\.edu$", email, re.IGNORECASE):
     print("Valid")
 else:
     print("Invalid")
