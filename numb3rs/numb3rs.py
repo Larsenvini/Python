e9fc118c1643388064d90dca30e691c089630d4c
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-10 19:28:34 +0000

    Wed, Jul 10, 2024, 4:28 PM -03:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -10,7 +10,7 @@ def validate(ip):
 
     ns = r'[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-1][0-9]|22[0-5]'
 
-    if re.search(fr'^(?:{ns}.{ns}.{ns}.{ns})$'):
+    if re.search(fr'^(?:{ns}\.{ns}\.{ns}\.{ns})$'):
         return "Valid"
     else:
         return "Invalid"
