37af7e12c8e09fb7193b4b85373d0206c77253b4
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-10 19:34:30 +0000

    Wed, Jul 10, 2024, 4:34 PM -03:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -2,13 +2,15 @@ import re
 import sys
 
 def main():
+    if len(sys.argv) != 2:
+        sys.exit
     ipv4 = sys.argv[1]
     print(validate(ipv4))
 
 
 def validate(ip):
 
-    ns = r'[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-1][0-9]|22[0-5]'
+    ns = r'([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-1][0-9]|22[0-5])'
 
     pattern = (fr'^(?:{ns}\.{ns}\.{ns}\.{ns})$')
 
