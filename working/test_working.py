30c759b513e206c648d535cad23d9a426e31b020
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-11 21:37:01 +0000

    Thu, Jul 11, 2024, 6:37 PM -03:00

--- working/test_working.py
+++ working/test_working.py
@@ -2,10 +2,10 @@ from working.working import convert
 import pytest
 
 def test_convert():
-    assert convert("9 AM TO 5 PM") == "09:00 to 17:00"
-    assert convert("8 AM TO 6 PM") == "08:00 to 18:00"
-    assert convert("10 AM TO 5 PM") == "10:00 to 17:00"
-    assert convert("5 AM TO 10 PM") == "05:00 to 10:00"
+    assert convert("9 AM to 5 PM") == "09:00 to 17:00"
+    assert convert("8 AM to 6 PM") == "08:00 to 18:00"
+    assert convert("10 AM to 5 PM") == "10:00 to 17:00"
+    assert convert("5 AM to 10 PM") == "05:00 to 10:00"
 
 def test_invalid_format():
     with pytest.raises(ValueError):
@@ -19,11 +19,11 @@ def test_invalid_format():
 
 def test_invalid_time():
     with pytest.raises(ValueError):
-        convert("12:60 AM - 5: PM")
+        convert("12:60 AM to 5:00 PM")
     with pytest.raises(ValueError):
-        convert("08:30 TO 17:30")
+        convert("08:80 AM to 17:30 PM")
     with pytest.raises(ValueError):
-        convert("6 AM 8 AM")
+        convert("19:00 AM to 8 PM")
     with pytest.raises(ValueError):
-        convert("8 AM until 22 PM")
+        convert("8:30:20 AM to 22 PM")
 
