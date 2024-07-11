ba208b7dea5837d7f510f8b4ceb1e7f21f158c21
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-11 15:37:03 +0000

    Thu, Jul 11, 2024, 12:37 PM -03:00

--- watch/watch.py
+++ watch/watch.py
@@ -13,8 +13,9 @@ def parse(s):
     match = re.search(r'src="(https?://(?:www\.)?youtube\.com/embed/[\w-]+)"', s)
 
     if match:
-        embed.url = match.group(1)
-        video
+        embed_url = match.group(1)
+        video_id = embed_url.split('/'[-1])
+        return f"
 
 
 
