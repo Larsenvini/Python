ca8fc953484afa7a9827de324a64cc7593f6bc8d
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-11 14:50:59 +0000

    Thu, Jul 11, 2024, 11:50 AM -03:00

---
+++ watch/watch.py
@@ -0,0 +1,19 @@
+import re
+import sys
+
+
+def main():
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python watch.py <html.address>")
+    html = sys.argv[1]
+    print(parse(html))
+
+def parse(s):
+    ...
+
+
+...
+
+
+if __name__ == "__main__":
+    main()
