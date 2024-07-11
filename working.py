446e25021d00c365d34d1e56fabe42499ce42f19
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-11 17:12:39 +0000

    Thu, Jul 11, 2024, 2:12 PM -03:00

--- working.py
+++ working.py
@@ -6,3 +6,8 @@ def main():
 def convert(hours):
 
     pattern = r'(\d{1,2}):?(\d{2})? (AM|PM) to (\d{1,2}):?(\d{2})? (AM|PM)'
+
+    match = re.fullmatch(pattern, hours)
+
+    if not match:
+        raise ValueError("Invalid Format")
