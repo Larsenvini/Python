0120d3f81dbcbcac1d741976fd8d276e7dbb215c
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-11 22:47:03 +0000

    Thu, Jul 11, 2024, 7:47 PM -03:00

--- um/um.py
+++ um/um.py
@@ -5,5 +5,15 @@ def main():
     print(count(input("Text: ")))
 
 def count(sentence):
+
+    um_count = 0
+
     pattern = re.search(r'[ um ]', sentence, re.IGNORECASE)
+
+    if pattern in sentence:
+        for pattern in sentence:
+            um_count += 1
+
+    return um_count
+
 
