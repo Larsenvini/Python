1dc1029554c67db0307aa324ab1dcc0ea2ac0521
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-12 21:35:14 +0000

    Fri, Jul 12, 2024, 6:35 PM -03:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,7 +1,7 @@
 import datetime
 
 def main():
-    print(birthdate(input("What's your birthday? ")))
+    print(birthdate(input("What's your birthday? Use YYYY-MM-DD ")))
 
 def birthdate(date):
-    
+
