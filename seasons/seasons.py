79e17d7ff316e8dae02ee47136c1045326bbc548
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-12 22:53:16 +0000

    Fri, Jul 12, 2024, 7:53 PM -03:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -5,10 +5,29 @@ def main():
     birthdate = get_birthdate()
     birthday1, birthday2 = birthdate.split(",", 1)
 
-    print(f"{birthdate}\n{birthday1} moments so dear\n{birthdate} minutes\nHow do you measure, measure a year?)
+    print(f"{birthdate}\n{birthday1} moments so dear\n{birthdate} minutes\nHow do you measure, measure a year?")
 
 def get_birthdate():
-          date_birth = input()
+          date_birth = input("What's your date of birth? Use YYYY-MM-DD ")
+          birth_date = datetime.strptime(date_birth, "%Y-%m-%d").date()
+
+          today = date.today()
+          delta = today - birth_date
+
+          minutes = delta.days * 24 * 60
+
+          minutes_in_words = number_to_words(minutes)
+
+          return minutes_in_words
+
+def number_to_words(number):
+    p = inflect.engine()
+    words = p.number_to_words(number)
+
+    words = words.replace("-", " ")
+    words = words.capitalize()
+
+    return words
 
 if __name__ == "__main__":
     main()
