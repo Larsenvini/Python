be59d5a91a4088472c47e133379b92b195acd4f5
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-12 16:57:20 +0000

    Fri, Jul 12, 2024, 1:57 PM -03:00

--- twitter.py
+++ twitter.py
@@ -2,4 +2,7 @@ import re
 
 url = input("Whats your twitter username? ").strip()
 
-re.search(r"^https?://(www\.)?twitter\.com/.+$", url, re.IGNORECASE)
+matches = re.search(r"^https?://(www\.)?twitter\.com/.+$", url, re.IGNORECASE)
+
+if matches:
+    print(f"Username: ", matches.group(1))
