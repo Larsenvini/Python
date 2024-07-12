89e23bae97acc26e02c0f10f84323151bbdacfd2
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-12 21:14:46 +0000

    Fri, Jul 12, 2024, 6:14 PM -03:00

--- student.py
+++ student.py
@@ -3,10 +3,18 @@ def main():
     print(f"{student['name']} from {student['house']}")
 
 def get_student():
-    student = {}
-    student['name'] = input("Name: ")
-    student['house'] = input("House: ")
-    return student
+    name = input("Name: ")
+    house = input("House: ")
 
+    return {"name": name, "house": house}
 if __name__ == "__main__":
     main()
+
+'''
+before = better for longer code:
+def get_student():
+    student = {}
+    student["name"] = input("Name: ")
+    student["house"] = input("House: ")
+    return student
+
