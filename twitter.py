798daced97a3084bb91a4da039265036992f77ec
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-12 16:42:01 +0000

    Fri, Jul 12, 2024, 1:42 PM -03:00

--- twitter.py
+++ twitter.py
@@ -2,7 +2,7 @@ import re
 
 url = input("Whats your twitter username? ").strip()
 
-username = re.sub(r"^(wwww\.)?https?://twitter\.com/", "", url)
+username = re.sub(r"^https?:)?(wwww\.)?//twitter\.com/", "", url)
 
 print(username)
 
