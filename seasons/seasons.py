3b3f666b5af8227b4a6cd143e7b966a8d64e21de
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-12 21:45:57 +0000

    Fri, Jul 12, 2024, 6:45 PM -03:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -3,9 +3,16 @@ from datetime import date
 def main():
     birthday = birthdate()
 
+    birthday1, birthday2  = birthday.split(",") #they have to be in words, with no and
+
+    print(f"{birthday1}{birthday2} minutes
+{birthday1} moments so dear
+{birthday1}{birthday2}
+How do you measure, measure a year?")
+
 def birthdate(birthday):
 
     date_birthday = (input("What's your birthday? Use YYYY-MM-DD "))
 
-    time_of_birth = datetime.year(date_birthday)
+    time_of_birth = date(date_birthday)
 
