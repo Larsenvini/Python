efbd27a840d3c35003eb43549953b6364f69b40a
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-12 22:38:08 +0000

    Fri, Jul 12, 2024, 7:38 PM -03:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,33 +1,10 @@
-from datetime import date
+from datetime import date, datetime
 
 def main():
-    birthday = birthdate()
+    birthdate = get_birthdate()
+    minutes = calculate_age_in_minutes(birthdate)
 
-    birthday1, birthday2  = birthday.split(",") #they have to be in words, with no and
-
-    print(f"{birthday1}{birthday2} minutes\n{birthday1} moments so dear\n{birthday1}{birthday2}\nHow do you measure, measure a year?")
-
-def birthdate():
-
-    date_birth = (input("What's your date of birth? Use YYYY-MM-DD "))# getting the birth date
-
-    year, month, day = date_birth.split("-")
-
-    d = date(year, month, day)
-
-    diff_minutes = year_to_minutes(d)
-
-    return diff_minutes
-
-def year_to_minutes(years):
-    today = date.today()
-
-
-    difference_in_days = (today - years).days
-
-    minutes = difference_in_days * 24 * 60
-
-    return minutes
+    print(f"{minutes})
 
 if __name__ == "__main__":
     main()
