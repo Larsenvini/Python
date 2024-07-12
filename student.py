27ad50112e8a36609a8e49f8cf6a50c36b4e9a1e
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-12 17:50:42 +0000

    Fri, Jul 12, 2024, 2:50 PM -03:00

--- student.py
+++ student.py
@@ -4,3 +4,11 @@ def main():
     house = get_house()
 
     print(f"{name} from {house}")
+
+def get_name():
+    return input("Name: ")
+
+def get_house():
+    return input("House: ")
+if __name__ == "__main__":
+    main()
