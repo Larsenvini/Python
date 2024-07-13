1fd2dbc8f743748724d1a6d9cb09210caa20833a
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-13 16:00:19 +0000

    Sat, Jul 13, 2024, 1:00 PM -03:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -5,7 +5,7 @@ def test_number_to_words():
     assert number_to_words(20) == "Twenty"
 
 @patch('builtins.input', return_value="2005-03-02")
-@patch('seasons.py')
+@patch('seasons.date')
 def test_get_birthdate(mock_date, mock_input):
     mock_date.today.return_value = date(2024, 7, 11)
     mock_date.side_effect = lambda *args, **kw: date(*args, **kw)
