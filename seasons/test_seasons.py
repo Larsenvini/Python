de41842c449f4e6273707ecac68ae8fbf87ade3b
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-13 15:59:16 +0000

    Sat, Jul 13, 2024, 12:59 PM -03:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -4,5 +4,13 @@ from seasons import number_to_words, get_birthdate
 def test_number_to_words():
     assert number_to_words(20) == "Twenty"
 
-def test_get_birthdate():
-    assert get_birthdate("2005-03-02") == "Ten million, one hundred eighty five thousand, one hundred twenty"
+@patch('builtins.input', return_value="2005-03-02")
+@patch('seasons.py')
+def test_get_birthdate(mock_date, mock_input):
+    mock_date.today.return_value = date(2024, 7, 11)
+    mock_date.side_effect = lambda *args, **kw: date(*args, **kw)
+
+    assert get_birthdate() == "Ten million, one hundred eighty five thousand, one hundred twenty"
+
+if __name__ == "__main__":
+    pytest.main()
