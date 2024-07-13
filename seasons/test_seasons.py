3d8e8d2265cfdfa546779e8982a5e72e662817ce
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-13 16:25:20 +0000

    Sat, Jul 13, 2024, 1:25 PM -03:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -8,7 +8,8 @@ def test_number_to_words():
     assert number_to_words(100) == "One hundred"
     assert number_to_words(1234) == "One thousand, two hundred thirty four"
 
-def test_get_birthdate():
+def test_get_birthdate(mock_date):
+    mock_date.today.return_value = date(2024, 7, 13)
     assert get_birthdate("2005-03-02") == "Ten million, one hundred eighty five thousand, one hundred twenty"
     assert get_birthdate("2020-01-01") == "Two million, three hundred eighty three thousand, two hundred"
     assert get_birthdate("2023-07-11") == "Five hundred twenty nine thousand, nine hundred twenty"
