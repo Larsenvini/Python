b9a075dfdafed829929c98abdfaba115e028305a
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-13 16:51:41 +0000

    Sat, Jul 13, 2024, 1:51 PM -03:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -3,9 +3,8 @@ from datetime import date, datetime
 
 def main():
     birthdate = get_birthdate()
-    birthday1, birthday2 = birthdate.split(",", 1)
 
-    print(f"{birthdate}\n{birthday1} moments so dear\n{birthdate} minutes\nHow do you measure, measure a year?")
+    print(f"{birthdate}\n moments so dear\n{birthdate} minutes\nHow do you measure, measure a year?")
 
 def get_birthdate(date_birth=None):
           if not date_birth:
