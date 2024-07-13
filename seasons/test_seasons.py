eb9e7298f8d6e5d1d953a15b6dd6fcb91efae7c8
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-13 15:23:48 +0000

    Sat, Jul 13, 2024, 12:23 PM -03:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -4,4 +4,5 @@ from seasons import number_to_words, get_birthdate
 def test_number_to_words():
     assert number_to_words(20) == "Twenty"
 
-def test_get_birthdate
+def test_get_birthdate():
+    assert get_birthdate() == "
