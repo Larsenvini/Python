73b848c977a7297a51e6390b5d3e8ccf0d931b6e
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-14 16:48:42 +0000

    Sun, Jul 14, 2024, 1:48 PM -03:00

--- student.py
+++ student.py
@@ -6,9 +6,10 @@ class Student():
         print(f"{student.name} from {student.house}")
 
         def get_student():
-            student = Student()
-            student.name = input("Name: ")
-            student.house = input("House: ")
+
+            name = input("Name: ")
+            house = input("House: ")
+            student = Student(name, house)
 
             return student
     if __name__ == "__main__":
