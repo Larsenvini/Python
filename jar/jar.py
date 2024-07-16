d1ab4d9b49366933acf34f2b811353363bf42af1
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-16 18:14:37 +0000

    Tue, Jul 16, 2024, 3:14 PM -03:00

--- jar/jar.py
+++ jar/jar.py
@@ -1,7 +1,11 @@
 class Jar:
     def __init__(self, capacity=12):
 
-        if not isinstance(capacity, int) or capacity < 0
+        if not isinstance(capacity, int) or capacity < 0: #isinstance = second evalue is instance of first
+            raise ValueError("Capacity must be a non-negative integer")
+
+        self._capacity = capacity
+        self._cookies = 0 #start at 0
 
     def __str__(self):
 
