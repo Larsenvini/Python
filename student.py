88d1cf2eca0ba15d7d7f677925b2d8ff329a40a2
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-16 00:12:25 +0000

    Mon, Jul 15, 2024, 9:12 PM -03:00

--- student.py
+++ student.py
@@ -1,11 +1,14 @@
 class Wizard:
-    def __init__(self, name, house):
-        if n
+    def __init__(self, name):
+        if not name:
+            raise ValueError("Missing name")
+        self.name = name
+
 class Student:
     def __init__(self, name, house):
      #self gets the just created object (Student()
 
-        self.name = name
+
         self.house = house
 
 
@@ -14,9 +17,7 @@ class Student:
 
 class Professor:
     def __init__(self, name, subject):
-        if not name:
-            raise ValueError("Missing name")
-        self.name = name
+       
         self.subject = subject
 
         ...
