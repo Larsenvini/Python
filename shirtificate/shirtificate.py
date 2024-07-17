3c0c7caf2165b2137163ccd1d95db691c003830d
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-17 15:36:29 +0000

    Wed, Jul 17, 2024, 12:36 PM -03:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -40,13 +40,21 @@ class PDF(FPDF):
         self.cell(text_width,  10, text, align="C")
 
 def download_image(url, save_path):
-    
+    response = requests.get(url)
+    image = Image.open(BytesIO(response.content))
+    image.save(save_path)
+
+image_url = "https://cs50.harvard.edu/python/2022/psets/8/shirtificate/shirtificate.png"
+image_path = "shirtificate.png"
+download_image(image_url, image_path)
+
 text = input("Name: ")
 final_text = (f"{text} took CS50")
+
 pdf = FPDF()
 pdf.add_page()
 pdf.set_font("helvetica", size=12) # add "B" or "I" before the size for bold/italic
-pdf.add_centered_image("https://cs50.harvard.edu/python/2022/psets/8/shirtificate/shirtificate.png")
+pdf.add_centered_image(image_path)
 pdf.add_centered_text(final_text)
 pdf.output("shirtificated.pdf")
 
