9d83654d5e209c67100b681074e1b7c9c0b68074
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-17 13:16:17 +0000

    Wed, Jul 17, 2024, 10:16 AM -03:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -2,7 +2,11 @@ from fpdf import FPDF
 
 class PDF(FPDF):
     def header(self):
-        self.set_font
+        self.set_font("helvetica", "B", 15)
+        self.cell(80)
+        self.cell(30,10, "CS50 Shirtificate", border =1, align="C")
+        self.ln(20)
+        
 pdf = FPDF()
 pdf.add_page()
 pdf.set_font("helvetica", size=12) # add "B" or "I" before the size for bold/italic
