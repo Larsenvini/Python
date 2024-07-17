e42a1d2d11852de1cc8fa3d943a4b1f294751498
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-17 13:25:41 +0000

    Wed, Jul 17, 2024, 10:25 AM -03:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -6,7 +6,10 @@ class PDF(FPDF):
         self.cell(80)
         self.cell(30,10, "CS50 Shirtificate", border =1, align="C")
         self.ln(20)
+
+    def add_centered_img(self, image_path):
         
+
 pdf = FPDF()
 pdf.add_page()
 pdf.set_font("helvetica", size=12) # add "B" or "I" before the size for bold/italic
