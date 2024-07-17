6712eb39f453687a615fddfc9b36214c09c63893
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-17 12:56:38 +0000

    Wed, Jul 17, 2024, 9:56 AM -03:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -3,6 +3,6 @@ from fpdf import FPDF
 pdf = FPDF()
 pdf.add_page()
 pdf.set_font("helvetica", size=12) # add "B" or "I" before the size for bold/italic
-pdf.cell(text="hello world")
+pdf.cell(text="hello world") # 
 pdf.output("hello_world.pdf")
 pdf = FPDF(orientation="P", unit="mm", format="A4")
