0e3ea2f24651706815a5f7d35c432c3510565f3b
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-17 14:05:00 +0000

    Wed, Jul 17, 2024, 11:05 AM -03:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -18,9 +18,27 @@ class PDF(FPDF):
         self.image(image_path, x, y, image_width, image_height)
 
     def get_image_dimensions(self, image_path):
+        from PIL import Image
+        image = Image.open(iamge_path)
+        width, height = image.size
+        aspect_ratio = height / width
+        return 100, 100 * aspect_ratio
+
+    def add_centered_text(self, text):
+        page_width = self.w - 2 * self.l_margin
+        text_width = self.get_string_width(text)
+        x = (page_width - text_width) / 2
+
+        page_height = self.h - 2 * self.t_margin
+        y = page_height / 2
+
+        self.set_xy(x,y)
+        self.set_font("helvetica", "B", 24)
+        self.cell(text_width,  10, text, align="C")
+
 pdf = FPDF()
 pdf.add_page()
 pdf.set_font("helvetica", size=12) # add "B" or "I" before the size for bold/italic
-pdf.cell(text="hello world") # specify dimensions before tex with 40, 10 example
+pdf.add_centered_image(https://cs50.harvard.edu/python/2022/psets/8/shirtificate/shirtificate.png)
 pdf.output("hello_world.pdf")
 pdf = FPDF(orientation="P", unit="mm", format="A4")
