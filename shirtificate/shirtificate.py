571ea18179af2d5fc8cd3f3b5e085ccf09fdefed
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-17 16:12:57 +0000

    Wed, Jul 17, 2024, 1:12 PM -03:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -6,7 +6,7 @@ from PIL import Image
 class PDF(FPDF):
     def header(self):
         self.set_font("helvetica", "B", 25)
-        self.cell(30,10, "CS50 Shirtificate", border =1, align="C")
+        self.cell(30,10, "CS50 Shirtificate", align="C")
         self.ln(20)
 
     def add_centered_image(self, image_path):
