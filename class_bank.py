6913dcd6a80e5be47ef77d1ad5481341f9fe4461
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-18 14:33:43 +0000

    Thu, Jul 18, 2024, 11:33 AM -03:00

--- class_bank.py
+++ class_bank.py
@@ -1,1 +1,16 @@
-class Account
+class Account:
+    def __init__(self):
+        self._balance = 0
+
+    @property
+    def balance(self):
+        return self._balance
+
+    def deposit(self, n):
+        self._balance += n
+
+    def withdraw(self, n):
+        self._balance -= n
+
+def main():
+    print("Balance: ", self._balance)
