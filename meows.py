2769211312e7e9fc8d496b50c7b1a162c520e6e0
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-19 15:00:17 +0000

    Fri, Jul 19, 2024, 12:00 PM -03:00

--- meows.py
+++ meows.py
@@ -2,6 +2,22 @@ def meow(n: int):
     for _ in range(n):
         return n * "meow\n"
 
+"""
+
+Meow n times
+
+:param n: Number of times to meow
+
+:type n: int
+
+:raise TypeError: If n is not an int
+
+:return: A string of n meows, one per line
+
+:rtype: str
+
+"""
+
 number: int = int(input("Number? "))
 meows: str = meow(number)
 print(meows, end="")
