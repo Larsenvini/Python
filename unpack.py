3527bf52b444c97424f435b11e98b7860dc2adc2
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-20 20:53:39 +0000

    Sat, Jul 20, 2024, 5:53 PM -03:00

--- unpack.py
+++ unpack.py
@@ -7,6 +7,6 @@ coins = {"galleons":100, "sickles": 50 ,"knuts": 25}
 print(total(**coins), "Knuts")
 """
 def f(*args, **kwargs):
-    print("Positionsl:", kwargs)
+    print("Named:", kwargs)
 
 f(galleons = 100, sickles = 50, knuts = 25, pennies = 5)
