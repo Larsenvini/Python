c4186bda5ec68d0985f1831563dfb2428e00213f
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-20 20:43:19 +0000

    Sat, Jul 20, 2024, 5:43 PM -03:00

--- unpack.py
+++ unpack.py
@@ -1,7 +1,9 @@
+"""
 def total(galleons, sickles, knuts):
     return (galleons * 17 + sickles) * 29 + knuts
 #unpacking = * for lists, ** for dicts
 coins = {"galleons":100, "sickles": 50 ,"knuts": 25}
 
 print(total(**coins), "Knuts")
-
+"""
+def f(*)
