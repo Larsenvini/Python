d5b76a0e9c5d9691298e61cbe06b0fd7c92492e5
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-20 21:08:31 +0000

    Sat, Jul 20, 2024, 6:08 PM -03:00

--- yell.py
+++ yell.py
@@ -2,9 +2,14 @@ def main():
     yell("opa bebe vamo ali")
 
 def yell(*words):
+    """
+    before:
     uppercased = []
     for word in words:
         uppercased.append(word.upper())
+        after:
+        """
+    uppercased = map(str.upper, words)
     print(*uppercased)
 
 main()
