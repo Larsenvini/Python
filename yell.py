70f01a04c200199bb16cb0f29576ea93a1ea0da8
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-20 21:05:36 +0000

    Sat, Jul 20, 2024, 6:05 PM -03:00

--- yell.py
+++ yell.py
@@ -1,4 +1,10 @@
-def yell():
-    r = input(": ").upper()
-    print(r)
-yell()
+def main():
+    yell("opa bebe vamo ali")
+
+def yell(*words):
+    uppercased = []
+    for word in words:
+        uppercased.append(word.upper())
+    print(*uppercased)
+
+main()
