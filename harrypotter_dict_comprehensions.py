8fc0a08d9cfdfc8e0aa34cc67ccb5832cea25b14
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-21 00:57:07 +0000

    Sat, Jul 20, 2024, 9:57 PM -03:00

--- harrypotter_dict_comprehensions.py
+++ harrypotter_dict_comprehensions.py
@@ -6,5 +6,7 @@ gryffindors = []
 for student in students:
     gryffindors.append({"name": student, "house": "Gryffindor"})
 """
-gryffindors = [{"name": student, "house": "Gryffindor"} for student in students]
+# this would be a list comprehension gryffindors = [{"name": student, "house": "Gryffindor"} for student in students]
+#dict comprehension:
+gryffindors = {student: "Gryffindor" for student in students}
 print(gryffindors)
