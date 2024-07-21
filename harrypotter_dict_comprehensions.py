96bb03d80a3e92bae9740225c9db327fc5557018
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-21 00:54:42 +0000

    Sat, Jul 20, 2024, 9:54 PM -03:00

--- harrypotter_dict_comprehensions.py
+++ harrypotter_dict_comprehensions.py
@@ -1,8 +1,10 @@
 students = ["Hermione", "Harry", "Ron"]
-
+"""
+before:
 gryffindors = []
 
 for student in students:
     gryffindors.append({"name": student, "house": "Gryffindor"})
-
-    
+"""
+gryffindors = [{"name": student, "house": "Gryffindor"} for student in students]
+print(gryffindors)
