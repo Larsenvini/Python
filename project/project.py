471d9ec4645456bfe5e6dc838acdb9aa5012593a
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-22 15:13:00 +0000

    Mon, Jul 22, 2024, 12:13 PM -03:00

---
+++ project/project.py
@@ -0,0 +1,21 @@
+## Code for the Final Project CS50P - Vinicius Larsen Santos
+
+
+def main():
+    ...
+
+
+def get_email():
+    ...
+
+def store_email():
+    ...
+
+def search_email():
+    ...
+
+def send_email():
+    ...
+
+def resend_email():
+    ...
