e950fa6b6751bc241974f8806140c9c206583bfb
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-26 18:19:21 +0000

    Fri, Jul 26, 2024, 3:19 PM -03:00

--- project/project.py
+++ project/project.py
@@ -4,8 +4,8 @@ import resend
 resend.api_key = os.environ["RESEND_API_KEY"]
 
 params: resend.Emails.SendParams = {
-    "from": "Acme <onboarding@resend.dev>",
-    "to": ["delivered@resend.dev"],
+    "from": "Acme <devtesting2003@gmail.com>",
+    "to": ["larsenvinicius8@gmail.com"],
     "subject": "hello world",
     "html": "<strong>it works!</strong>",
 }
