c73e6fc45641c1a4a2b36af2d524cbc565f4871e
Larsenvini <135841562+Larsenvini@users.noreply.github.com>
2024-07-29 16:32:52 +0000

    Mon, Jul 29, 2024, 1:32 PM -03:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -4,6 +4,7 @@ from plates import is_valid
 def test_valid_plate1():
 
     assert is_valid("CS50") == True
+    
 def test_valid_plate2():
     assert is_valid("HUBA7") == True
 
@@ -29,8 +30,12 @@ def test_invalid_plate8():
 def test_invalid_plate9():
     assert is_valid("CS.") == False
 
-def test_beggining():
-    assert is_valid
+def test_plate_without_beginning_alphabetical_check():
+    assert is_valid("1ABCD") == False
+
+def test_plate_without_zero_placement_check():
+    assert is_valid("AB01") == False
+
 if __name__ == "__main__":
     pytest.main()
 
